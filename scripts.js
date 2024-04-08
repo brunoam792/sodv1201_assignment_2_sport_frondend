4c2b3bc1cf35cee6a67b88b907e86fb05581104a
brunoam792 <brunoam792@gmail.com>
2024-04-07 21:04:54 -0400

    First commit

---
+++ scripts.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadForm();
+    const form = document.getElementById('registrationForm');
+});
+
+async function loadForm() {
+    try {
+        const response = await fetch('http://localhost:3000/form-html');
+        if (!response.ok) {
+            throw new Error('Failed to fetch form HTML');
+        }
+        const formHtml = await response.text();
+        document.getElementById('formContainer').innerHTML = formHtml;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function submitForm(e) {
+    e.preventDefault();
+    const form = document.getElementById('registrationForm');
+    const confirmation = document.getElementById('confirmation');
+    const formData = new FormData(form);
+    const responseData = await fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.fromEntries(formData))
+    }).then(response => response.json());
+
+    // Hide the form
+    form.style.display = 'none';
+
+    // Show only the confirmation section
+    confirmation.style.display = 'block';
+    document.getElementById('name').innerText = responseData.registrationData.fullName;
+    document.getElementById('fee').innerText = responseData.registrationData.fee;
+    
+    return false; // Prevent form submission
+}
